634cb20665044e2630a589bfc3aeb939e0cb6f82
Ronald Wong <ronaldchunyin@gmail.com>
2018-06-17 14:34:48 +0800

    init

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
 	production: false,
 	firebase: {
-		apiKey: "AIzaSyCZd2-rC0pdC8CAvUOafCQSZulFYcTTl3g",
+		apiKey: "xxx",
 		authDomain: "cloud-firestore-test-22758.firebaseapp.com",
 		databaseURL: "https://cloud-firestore-test-22758.firebaseio.com",
 		projectId: "cloud-firestore-test-22758",
